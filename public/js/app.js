d6822766b09c1827b5c4c214f498b0bf7d226b3d
Laura <lg3295@gmail.com>
2016-04-04 10:39:04 -1000

    receives data

---
+++ public/js/app.js
@@ -0,0 +1,29 @@
+var searchBtn = document.getElementById('searchBtn');
+searchBtn.addEventListener('click', function(event){
+  event.preventDefault();
+
+  var searchInput = document.getElementById('searchInput').value;
+  var searchTitle = document.getElementById('searchTitle');
+  searchTitle.innerHTML = searchInput;
+
+  var imagesRequest = new XMLHttpRequest();
+  imagesRequest.addEventListener('load', getResults);
+  imagesRequest.open('GET', "https://api.gettyimages.com/v3/search/images?phrase=" + searchInput);
+  imagesRequest.setRequestHeader('Api-Key', 'uxbmd9yj85eks5wfjmqn3rev');
+  imagesRequest.send();
+});
+
+var displayDiv = document.getElementById('displayDiv');
+
+function getResults(){
+// Clear User Input
+  userInput = '';
+
+
+  var allTheResults = JSON.parse(this.responseText);
+
+
+
+};
+
+//picture: displayDiv.innerHTML = allTheResults.images[0].display_sizes[0].uri;
